25343fd04390bfeefed3f6ef5caf6d6f7fa9039d
jonrhymes <63318712+jonrhymes@users.noreply.github.com>
2020-05-02 18:52:20 -0400

    commit

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@
 const global = {
     suits = ['diamonds', 'hearts', 'clubs', 'spades'],
     values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'],
-}
+};
 
 
 console.log(suits[1])
