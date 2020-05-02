065f5546fd2f5432f49121f618d435ae40d7cea3
jonrhymes <63318712+jonrhymes@users.noreply.github.com>
2020-05-02 19:25:49 -0400

    commit

--- js/app.js
+++ js/app.js
@@ -8,6 +8,15 @@
 const suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
 const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
 
+class Card {
+    constructor(suits, values) {
+        this.suits = suits;
+        this.values = values;
+    }
+}
+
+console.log(Card.suits)
+
 /////////////
 // Functions
 
