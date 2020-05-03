cbcf2c68576fd4c28fe6420256676015a1b30b81
jonrhymes <63318712+jonrhymes@users.noreply.github.com>
2020-05-03 10:30:10 -0400

    commit

--- js/app.js
+++ js/app.js
@@ -5,17 +5,85 @@
 ///////////////////
 // Global Variables
 
+// ==========> 
+// "Building a deck of cards in Javascript"
+// by Adam Coder
+// https://www.youtube.com/watch?v=h3ehO5QMNu4 
+// ===========>
+
 const suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
 const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
 
+// Card class
 class Card {
-    constructor(suits, values) {
-        this.suits = suits;
-        this.values = values;
+    constructor(suit, value) {
+        this.suit = suit;
+        this.value = value;
+        this.name = value + ' of ' + suit;
     }
 }
 
-console.log(Card.suits)
+let card = new Card('2','Diamonds');
+
+// Deck class
+class Deck {
+    constructor() {
+        this.deck = [];
+    }
+
+    createDeck(suits, values) {
+        for(let suit of suits) {
+            for(let value of values) {
+                this.deck.push(new Card(suit, value));
+            }
+        }
+        return this.deck;
+    }
+    shuffle() {
+        let counter = this.deck.length, temp, i;
+
+        while(counter) {
+            i = Math.floor(Math.random() * counter--); 
+            temp = this.deck[counter];
+            this.deck[counter] = this.deck[i];
+            this.deck[i] = temp;
+
+// ======> 
+// Flip indices
+
+// let arr = [1, 2]; // arr equals [1, 2]
+// let temp = arr[0]; // temp equals 1
+// arr[0] = arr[1]; // sets arr[0] to 2. arr = [2, 2]
+// arr[1] = temp; // sets arr[1] to 1. now arr = [2, 1]
+
+// ======>
+
+        }
+        return this.deck;
+    }
+
+    deal () {
+        let hand = [];
+        while(hand.length < 11) {
+            hand.push(this.deck.shift());
+        } 
+        // ^ Takes 11 cards from the bottom of the deck
+        return hand;
+    }
+
+    clearDeck () {
+        this.deck = [];
+    }
+}
+
+let deck = new Deck();
+deck.createDeck(suits, values);
+
+console.log(deck);
+console.log(deck.createDeck(suits, values))
+console.log(deck.shuffle());
+console.log(deck.deal());
+console.log(deck.clearDeck());
 
 /////////////
 // Functions
