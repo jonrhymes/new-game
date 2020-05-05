bf689f5cbce6c162ae8bc157d9c0f6ad685128bf
jonrhymes <63318712+jonrhymes@users.noreply.github.com>
2020-05-05 11:47:48 -0400

    12pm commit

--- js/app.js
+++ js/app.js
@@ -1,94 +1,108 @@
 // console.log("I'm working!");
 // console.log($);
 
-
-///////////////////
-// Global Variables
-
 // ==========> 
 // Sources: 
 // 1. "Building a deck of cards in Javascript" by Adam Coder, https://www.youtube.com/watch?v=h3ehO5QMNu4 
 
-// 2. Game model adapated from Tyler Roland's War, https://github.com/MisterTeeRoland/war
+// 2. Game adapated from Tyler Roland's War, https://github.com/MisterTeeRoland/war
 
 // ===========>
 
-const suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
-const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+///////////////////
+// Global Variables
+
+// Declares empty strings and arrays
+let playerDeck = '';
+let compDeck = '';
+let playerCard = '';
+let compCard = '';
+let playerHand = [];
+let compHand = [];
+
+const deck = [
+    {name: '2 of Clubs', img: 
+]
 
 
-// Card class
-class Card {
-    constructor(suit, value) {
-        this.suit = suit;
-        this.value = value;
-        this.name = value + ' of ' + suit;
-    }
-}
+// const suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+// const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
 
-let card = new Card('2','Diamonds');
 
-/////////////
-// Functions
+// // Card class
+// class Card {
+//     constructor(suit, value) {
+//         this.suit = suit;
+//         this.value = value;
+//         this.name = value + ' of ' + suit;
 
-// Deck class
-class Deck {
-    constructor() {
-        this.deck = [];
-    }
+//     }
+// }
 
-    createDeck(suits, values) {
-        for(let suit of suits) {
-            for(let value of values) {
-                this.deck.push(new Card(suit, value));
-            }
-        }
-        return this.deck;
-    }
-    shuffle() {
-        let counter = this.deck.length, temp, i;
+// let card = new Card('2','Diamonds');
 
-        while(counter) {
-            i = Math.floor(Math.random() * counter--); 
-            temp = this.deck[counter];
-            this.deck[counter] = this.deck[i];
-            this.deck[i] = temp;
 
-// ======> 
-// Flip indices
+// /////////////
+// // Functions
 
-// let arr = [1, 2]; // arr equals [1, 2]
-// let temp = arr[0]; // temp equals 1
-// arr[0] = arr[1]; // sets arr[0] to 2. arr = [2, 2]
-// arr[1] = temp; // sets arr[1] to 1. now arr = [2, 1]
+// // Deck class
+// class Deck {
+//     constructor() {
+//         this.deck = [];
+//     }
 
-// ======>
+//     createDeck(suits, values) {
+//         for(let suit of suits) {
+//             for(let value of values) {
+//                 this.deck.push(new Card(suit, value));
+//             }
+//         }
+//         return this.deck;
+//     }
+//     shuffle() {
+//         let counter = this.deck.length, temp, i;
 
-        }
-        return this.deck;
-    }
+//         while(counter) {
+//             i = Math.floor(Math.random() * counter--); 
+//             temp = this.deck[counter];
+//             this.deck[counter] = this.deck[i];
+//             this.deck[i] = temp;
 
-    deal () {
-        let hand = [];
-        while(hand.length < 26) {
-            hand.push(this.deck.shift());
+// // ======> 
+// // Flip indices
 
-        } 
-        // ^ Takes 26 cards from the bottom of the deck
-        return hand;
+// // let arr = [1, 2]; // arr equals [1, 2]
+// // let temp = arr[0]; // temp equals 1
+// // arr[0] = arr[1]; // sets arr[0] to 2. arr = [2, 2]
+// // arr[1] = temp; // sets arr[1] to 1. now arr = [2, 1]
 
-    }
+// // ======>
+
+//         }
+//         return this.deck;
+//     }
+
+//     deal () {
+//         let hand = [];
+//         while(hand.length < 26) {
+//             hand.push(this.deck.shift());
+
+//         } 
+//         // ^ Takes 26 cards from the bottom of the deck
+//         return hand;
+
+//     }
 
     
-}
+// }
 
-let deck = new Deck();
-deck.createDeck(suits, values);
+// let deck = new Deck();
+// deck.createDeck(suits, values);
 
-console.log(deck);
-// console.log(deck.createDeck(suits, values))
-console.log(deck.shuffle());
-console.log(deck.deal());
+// console.log(deck);
+// // console.log(deck.createDeck(suits, values))
+// console.log(deck.shuffle());
+// console.log(deck.deal());
 
 
 
@@ -96,28 +110,21 @@ console.log(deck.deal());
 /////////////////
 // Event Handlers
 
-// const handle = {
-//     click: () => console.log("hello"),
-//     addCard: () => {
-
-//     }
-// }
-
-// const addCard = () => {
-//     const $img = $('<img>');
-//     $img.attr('src', randomCard());
-//     $body.append($img)
-// };
-
-// const imager = {
-//     click: () => {
-//         let $img = $('<img>');
-//         $img.attr('src', )
-//     }
-// }
 
 
 ////////////
 // App Logic
 
+const $button = $('.btn');
+const $playerDeck = $('.playerDeck')
+const $compDeck = $('.compDeck')
+// const $img1 = $('<img>')
+
+$button.on('click', () => {
+    $playerDeck.append($('<img class="front-card">').attr('src','img/0.png'))
+})
+
+$button.on('click', () => {
+    $compDeck.prepend($('<img class="front-card">').attr('src','img/31.png'))
+})
 
